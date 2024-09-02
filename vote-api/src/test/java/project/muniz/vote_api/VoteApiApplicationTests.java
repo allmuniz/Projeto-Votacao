1d6c590af45d10620d6b7468ca16271487d605ce
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2024-09-02 12:33:49 -0300

    First update

---
+++ vote-api/src/test/java/project/muniz/vote_api/VoteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package project.muniz.vote_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
